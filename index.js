8562d2136526371712ef6471ab13869002462290
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-21 14:15:47 -0500

    Modificando el console.log para imprimir solo los nombres de los MC's

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const carlo = new MissionCommander("Carlo");
 const fernanda = new MissionCommander("Fernanda");
 const rodrigo = new MissionCommander("Rodrigo");
 
-console.log(carlo);
-console.log(fernanda);
-console.log(rodrigo);
+console.log(carlo.nombre);
+console.log(fernanda.nombre);
+console.log(rodrigo.nombre);
