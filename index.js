2c831e91974c5c82419a79a006dcad8263223126
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-21 13:40:57 -0500

    Agregando el archivo principal (index)

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require("./app/missionCommander");
+
+const carlo = new MissionCommander("Carlo");
+const fernanda = new MissionCommander("Fernanda");
+const rodrigo = new MissionCommander("Rodrigo");
+
+console.log(carlo);
+console.log(fernanda);
+console.log(rodrigo);
